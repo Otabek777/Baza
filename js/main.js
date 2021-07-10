b410e04a98f407f90bb6ec4deb30bf0369c25e51
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-07-10 23:41:32 +0500

    finish

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 document.querySelector('.header__navbar .btn').addEventListener('click', function() {
     this.classList.toggle('active');
 });
+
+document.querySelector('.header__burgir').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.navbar__block').classList.toggle('active');
+});
